c23f6362ce89dccdf427239de27777c138655190
Radek <92121154+radekchybowski@users.noreply.github.com>
2024-08-25 01:45:06 +0200

    library sort of works, minor changes to date display

--- src/pages/CategoriesPage.jsx
+++
@@ -1,9 +0,0 @@
-import Section from "@/components/ui/section";
-
-const CategoriesPage = () => {
-  return (
-    <Section title="Categories" fetch='categories' items={30} component='categories'/>
-  );
-};
-
-export default CategoriesPage;
